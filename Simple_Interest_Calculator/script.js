45c882959b4710f548e448884a0b7dc551443c8d
TejasNayak42 <tejasnayak.c42@gmail.com>
2023-05-06 12:57:34 +0530

    Uploaded

---
+++ Simple_Interest_Calculator/script.js
@@ -0,0 +1,25 @@
+function updaterate(){
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function compute(){
+    var principal=document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest=principal * years * rate / 100;
+
+    var year=new Date().getFullYear() + parseInt(years);
+    var Amount=parseInt(principal) + parseFloat(interest);
+    var result=document.getElementById("result");
+
+    if(principal<=0){
+        alert("Please enter a positive number !");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + Amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    }
+
+}
